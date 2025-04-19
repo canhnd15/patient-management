4ed96baa0f5bfb4972999a69413c8dc640a8f838
canhnd <canhnd15@gmail.com>
2025-04-19 10:36:29 +0700

    add grpc billing service

--- billing-service/src/main/java/com/davidnguyen/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/davidnguyen/billingservice/grpc/BillingGrpcService.java
@@ -1,8 +1,6 @@
 package com.davidnguyen.billingservice.grpc;
 
-import billing.BillingRequest;
-import billing.BillingResponse;
-import billing.BillingServiceGrpc;
+import billing.*;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
@@ -12,6 +10,8 @@ import org.slf4j.LoggerFactory;
 public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
 
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    private final Integer BALANCE = 1_500_0000;
 
     @Override
     public void createBillingAccount(BillingRequest billingRequest,
@@ -26,5 +26,25 @@ public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBas
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getBillingByAccountId(AccountInfoRequest accountInfoRequest,
+                                      StreamObserver<BillingByAccountResponse> responseObserver) {
+
+        String code = accountInfoRequest.getAccountCode();
+        int amount = (int) accountInfoRequest.getAmount();
+
+        if ("ADX134".equals(code)) {
+            BillingByAccountResponse response = BillingByAccountResponse.newBuilder()
+                    .setAccountId("123")
+                    .setAccountCode("ADX134")
+                    .setAccountEmail("test@example.com")
+                    .setBalance(BALANCE - amount)
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
     }
 }
